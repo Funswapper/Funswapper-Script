86fef3f51e3554cf44fc65bb58580224d5bd190b
Funswapper <ddsfsdf600@gmail.com>
2024-11-09 01:11:56 +0300

    Update Status.lua

--- Status.lua
+++ Status.lua
@@ -1,12 +1,12 @@
 --This may save you from a ban if Funswapper will get detected on the following games
 
 local FSStatus = {
-  301549746 = "allow"; --CB
-  1480424328 = "allow"; --CB Competitive
-  1869597719 = "allow"; --CB Team Deathmatch
-  5325113759 = "disallow"; --CB Trading
-  84957635803107 = "allow"; --CB Source 2 Star Studios Forever
-  17586052747 = "allow"; --CB Reimagined V2 DEVR0LEX
+	["301549746"] = "allow"; --CB
+	["1480424328"] = "allow"; --CB Competitive
+	["1869597719"] = "allow"; --CB Team Deathmatch
+	["5325113759"] = "disallow"; --CB Trading
+	["84957635803107"] = "allow"; --CB Source 2 Star Studios Forever
+	["17586052747"] = "allow"; --CB Reimagined V2 DEVR0LEX
 }
 
 return FSStatus
